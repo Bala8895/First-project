ca150dbfc4db0fbb0c64476de2ea00130993c302
Balaji Selvaraj <157351630+Bala8895@users.noreply.github.com>
2024-04-07 09:52:55 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker login -u bala0808 -p dckr_pat_-CYnYWKTEzbzfIsH60iP5egI3uc
 # Tag the image with the Docker Hub repository name and version
-if [[ $GIT_BRANCH == 'origin/dev' ]]; then
+if [[ $GIT_BRANCH == 'origin/main' ]]; then
 docker tag project:v1 bala0808/prod
 docker push bala0808/prod
 # Docker login
